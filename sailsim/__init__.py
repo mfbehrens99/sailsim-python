128324733c80c86cdf16924f390085ccee165f33
Michael Behrens <mfbehrens99@gmail.com>
2020-12-31 11:11:45 +0100

    Added small test script

--- sailsim/__init__.py
+++ sailsim/__init__.py
@@ -2,3 +2,13 @@ from simulation import Simulation
 from world import World
 from algorithmus import Algorithmus
 
+def simulationTest():
+    boat = Boat()
+    wf = Windfield(1, 0.5)
+    wind = Wind(wf)
+
+    world = World(boat,wind)
+
+    sim = Simulation(.1, world)
+
+    sim.runStep()
