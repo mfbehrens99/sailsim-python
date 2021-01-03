2aca318efa921f995282632acd8a7e7584d351d6
Michael Behrens <mfbehrens99@gmail.com>
2021-01-03 22:34:21 +0100

    implemented getWind

--- sailsim/world/Wind.py
+++ sailsim/world/Wind.py
@@ -1,19 +1,27 @@
+import math
+
 from world import Windfield
 
 class Wind:
     """Generates and calculates speed and direction of wind"""
 
     def __init__(self, sizeX, sizeY, winds):
-        # Size unnecessary?
+        # TODO Size unnecessary?
         self.sizeX = sizeX
         self.sizeY = sizeY
 
         self.winds = winds
 
-    def getWind(x, y):
-        # work with wind as a vector with x,y component rather than polar coordinates?
-        speed, direction
-        for wind in winds:
-            wind.getWind(x,y)
-            # TODO addition of winds
+    def getWind(self, x, y):
+        """Summs and returns the speed and direction of all windfields"""
+        sum_x = 0
+        sum_y = 0
+        for wind in self.winds:
+            (wind_x, wind_y) = wind.getWindCart(x, y)
+            sum_x += wind_x
+            sum_y += wind_y
+
+        # Convert to polar
+        speed = math.sqrt(x**2 + y**2)
+        direction = math.degrees(math.atan(y/x))
         return (speed, direction)
--- sailsim/world/Windfield.py
+++ sailsim/world/Windfield.py
@@ -1,6 +1,19 @@
+import math
+
 class Windfield:
     """Describes a partion of the wind"""
 
     def __init__(self, speed, direction):
         self.speed = speed
         self.direction = direction
+
+    def getWind(self, x, y):
+        """Returns direction and speed of the windfield at the position (x,y) as a tuple"""
+        return(self.speed,self.direction)
+
+    def getWindCart(self, x, y):
+        """Returns cartesian components of the windfield at the position (x,y) as a tuple"""
+        (speed, direction) = self.getWind(x, y)
+        x_cart = speed * math.cos(math.radians(direction))
+        y_cart = speed * math.sin(math.radians(direction))
+        return (x_cart,y_cart)
