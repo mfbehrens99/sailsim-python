502270abc55dfd476952f8465342452f620a9954
Michael Behrens <mfbehrens99@gmail.com>
2021-01-08 09:35:32 +0100

    Update testwind.py

--- tests/world/testwind.py
+++ tests/world/testwind.py
@@ -1,8 +1,35 @@
 import unittest
+from math import pi, sqrt
+
+from sailsim.world.Wind import Wind
+from sailsim.world.Windfield import Windfield
+
+ALMOST_GET_WIND = 8
 
 class TestWind(unittest.TestCase):
+    def setUp(self):
+        self.w = Wind([])
+
     def testGetWindCart(self):
-        pass
+        self.w.winds = []
+        self.assertEqual(self.w.getWindCart(0, 0, 0), (0, 0))
+        self.w.winds = [Windfield(1,0)]
+        self.assertEqual(self.w.getWindCart(0, 0, 0), (1, 0))
+        self.w.winds = [Windfield(1,0), Windfield(0,1)]
+        self.assertEqual(self.w.getWindCart(0, 0, 0), (1, 1))
+        self.w.winds = [Windfield(2,1), Windfield(4,1)]
+        self.assertEqual(self.w.getWindCart(0, 0, 0), (6, 2))
+
+    def testGetWind(self):
+        self.w.winds = [Windfield(1,0)]
+        self.assertAlmostEqual(self.w.getWind(0, 0, 0)[0],       1, ALMOST_GET_WIND)
+        self.assertAlmostEqual(self.w.getWind(0, 0, 0)[1],       0, ALMOST_GET_WIND)
+        self.w.winds = [Windfield(0,1)]
+        self.assertAlmostEqual(self.w.getWind(0, 0, 0)[0],       1, ALMOST_GET_WIND)
+        self.assertAlmostEqual(self.w.getWind(0, 0, 0)[1],    pi/2, ALMOST_GET_WIND)
+        self.w.winds = [Windfield(-1,1)]
+        self.assertAlmostEqual(self.w.getWind(0, 0, 0)[0], sqrt(2), ALMOST_GET_WIND)
+        self.assertAlmostEqual(self.w.getWind(0, 0, 0)[1],  5/4*pi, ALMOST_GET_WIND)
 
 if __name__ == '__main__':
     unittest.main()
