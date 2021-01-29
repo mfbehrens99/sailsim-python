c5ad2e111e252447b15d22481cfc4382b58b96fa
Michael Behrens <mfbehrens99@gmail.com>
2021-01-29 15:22:45 +0100

    Update Windfield.py

--- sailsim/world/Windfield.py
+++ sailsim/world/Windfield.py
@@ -1,17 +1,18 @@
 from sailsim.utils.coordconversion import cartToPolar
 
+
 class Windfield:
-    """Describes a partion of the wind"""
+    """Describe a partion of the wind."""
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.posX = x
+        self.posY = y
 
     def getWindCart(self, x, y, t):
-        """Returns cartesian components of the windfield at the position (x,y) as a tuple"""
-        return (self.x, self.y)
+        """Return cartesian components of the windfield at the position (x, y) as a tuple."""
+        return (self.posX, self.posY)
 
     def getWind(self, x, y, t):
-        """Returns direction and speed of the windfield at the position (x,y) as a tuple"""
+        """Return direction and speed of the windfield at the position (x, y) as a tuple."""
         (cartX, cartY) = self.getWindCart(x, y, t)
         return cartToPolar(cartX, cartY)
