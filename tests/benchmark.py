a707d056be28cb21f4c687a02d7f6bf9e88d2302
Michael Behrens <mfbehrens99@gmail.com>
2021-01-29 15:22:59 +0100

    Update benchmark.py

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -3,17 +3,21 @@ from sailsim.world.Wind import Wind
 from sailsim.world.Windfield import Windfield
 from sailsim.world.Squallfield import Squallfield
 
-#TODO this should test a simulation step
+
+# TODO this should test a simulation step
 
 class Benchmark:
+    """Test which module requires what partion of time when simulating."""
+
     def __init__(self):
-        wf = Windfield(3,2)
-        squallfield = Squallfield(3,2,2,1)
-        self.wind = Wind([wf, squallfield])
+        windField = Windfield(3, 2)
+        squallfield = Squallfield(3, 2, 2, 1)
+        self.wind = Wind([windField, squallfield])
 
     def run(self, number=100000):
         for i in range(number):
-            self.wind.getWind(0,0,i)
+            self.wind.getWind(0, 0, i)
+
 
 if __name__ == "__main__":
     import cProfile
