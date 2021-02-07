0f322ca0852199857f7dcb272e0e5ad9a9808efe
mfbehrens99 <mfbehrens99@gmail.com>
2021-02-07 17:29:17 +0100

    Create angleCalculations.py

---
+++ sailsim/utils/angleCalculations.py
@@ -0,0 +1,11 @@
+"""This module is intended for angle and direction calcualtions."""
+
+
+def angleKeepInterval(angle):
+    """Keep angle inside the range of [-pi; pi]."""
+    return angle
+
+
+def directionKeepInterval(direction):
+    """Keep direction inside the range of [0; 2*pi]."""
+    return direction
