ed98267ff29ce0aceb7bd4c94ab1a8047ed9fbd5
mfbehrens99 <mfbehrens99@gmail.com>
2021-02-07 10:52:41 +0100

    Update testsimulation.py

--- tests/testsimulation.py
+++ tests/testsimulation.py
@@ -10,14 +10,23 @@ from sailsim.world.Wind import Wind
 from sailsim.world.Windfield import Windfield
 from sailsim.world.Squallfield import Squallfield
 
-wf = Windfield(0, 0)
+wf = Windfield(10, 0)
 sqf = Squallfield(0, 0, 100, 1, 0)
 
 wind = Wind([wf, sqf])
-boat = Boat(0, 0, 100, 10, None)
+b = Boat(0, 0, 100, 10, None)
 
-w = World(boat, wind, None)
-
+# Create world and simulation
+w = World(b, wind, None)
 s = Simulation(w, 0.01, 2048)
 
+# Simulate 2 steps with print
+print(s)
+s.step()
+print(s)
+s.step()
+print(s)
+
+# Finish simulation
 s.run()
+print(s)
