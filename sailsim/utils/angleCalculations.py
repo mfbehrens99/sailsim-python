6768c524930a2ef757c69d0c92768a7691d5eda1
mfbehrens99 <mfbehrens99@gmail.com>
2021-02-08 22:15:14 +0100

    Implement angleKeepInterval(), directionKeepInterval()

--- sailsim/utils/angleCalculations.py
+++ sailsim/utils/angleCalculations.py
@@ -1,11 +1,21 @@
 """This module is intended for angle and direction calcualtions."""
 
+from math import pi
+
 
 def angleKeepInterval(angle):
     """Keep angle inside the range of [-pi; pi]."""
+    if angle > pi:
+        return angle - 2 * pi
+    if angle <= -pi:
+        return angle + 2 * pi
     return angle
 
 
 def directionKeepInterval(direction):
     """Keep direction inside the range of [0; 2*pi]."""
+    if direction >= 2 * pi:
+        return direction - 2 * pi
+    if direction < 0:
+        return direction + 2 * pi
     return direction
