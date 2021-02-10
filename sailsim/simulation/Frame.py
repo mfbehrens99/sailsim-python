9f16e0534e28df96482e28d405bb9ff81899ec78
mfbehrens99 <mfbehrens99@gmail.com>
2021-02-10 13:55:12 +0100

    Improved Frame and Framelist

--- sailsim/simulation/Frame.py
+++ sailsim/simulation/Frame.py
@@ -1,14 +1,33 @@
 class Frame():
     """This class is holding all data about one frame in the simulation."""
 
-    def __init__(self, time):
-        self.time = time
+    def __init__(self):
+        self.nr = self.time = None
+
         self.windTable = []
 
-        self.boatPosX = None
-        self.boatPosY = None
-        self.boatSpeedX = None
-        self.boatSpeedY = None
+        self.boatPosX = self.boatPosY = None
+        self.boatSpeedX = self.boatSpeedY = None
+        self.boatDirection = None
+
+        self.boatMainSailAngle = None
+        self.boatRudderAngle = None
+
+        self.boatApparentWindX = self.boatApparentWindY = None
+        self.boatApparentWindAngle = None
+        self.boatLeewayAngle = None
+        self.boatAngleOfAttack = None
+
+        self.boatForceX = self.boatForceY = None
+        self.boatSailDragX = self.boatSailDragY = None
+        self.boatSailLiftX = self.boatSailLiftY = None
+        self.boatWaterDragX = self.boatWaterDragY = None
+        self.boatWaterLiftX = self.boatWaterLiftY = None
+
+
+    def collectSimulation(self, simulation):
+        self.nr = simulation.frame
+        self.time = simulation.getTime()
 
     def collectBoat(self, boat):
         """Collect and save all information about the boat."""
@@ -16,19 +35,50 @@ class Frame():
         self.boatPosY = boat.posY
         self.boatSpeedX = boat.speedX
         self.boatSpeedY = boat.speedY
+        # self.boatDirection = boat.direction
+
+        self.boatMainSailAngle = boat.mainSailAngle
+        # self.boatRudderAngle = boat.rudderAngle
+
+        h = boat.dataHolder
+        (self.boatApparentWindX, self.boatApparentWindY) = (h.apparentWindX, h.apparentWindY)
+        self.boatApparentWindAngle = h.apparentWindAngle
+        self.boatLeewayAngle = h.leewayAngle
+        self.boatAngleOfAttack = h.angleOfAttack
+
+        (self.boatForceX, self.boatForceY) = (h.forceX, h.forceY)
+        (self.boatSailDragX, self.boatSailDragY) = (h.sailDragX, h.sailDragY)
+        (self.boatSailLiftX, self.boatSailLiftY) = (h.sailLiftX, h.sailLiftY)
+        (self.boatWaterDragX, self.boatWaterDragY) = (h.waterDragX, h.waterDragY)
+        (self.boatWaterLiftX, self.boatWaterLiftY) = (h.waterLiftX, h.waterLiftY)
 
     def collectWind(self, wind, x, y, size, distance):
         """Collect and save all information about the wind."""
-        self.windTable = []
+        windTable = []
         for i in range(-size, size + 1):
-            row = []
             for j in range(-size, size + 1):
                 coordX = x + i * distance
                 coordY = y + j * distance
-                row.append(wind.getWindCart(coordX, coordY, self.time))
-            self.windTable.append(row)
+                windTable.append(wind.getWindCart(coordX, coordY, self.time))
+        self.windTable = windTable
+
+    def getWindList(self):
+        windList = []
+        for wind in self.windTable:
+            windList.append(wind[0])
+            windList.append(wind[1])
+        return windList
 
     def getCSVLine(self):
         """Return string that contains all data about this frame."""
-        # TODO write toCSVLine
-        return "%s,%f,%f,%f,%f\n" % (self.time, self.boatPosX, self.boatPosY, self.boatSpeedX, self.boatSpeedY)
+        data = [
+            self.nr, self.time,
+            self.boatPosX, self.boatPosY, self.boatSpeedX, self.boatSpeedY, # self.boatDirection,
+            self.boatApparentWindX, self.boatApparentWindY, self.boatApparentWindAngle, self.boatLeewayAngle, self.boatAngleOfAttack,
+            self.boatForceX, self.boatForceY,
+            self.boatSailDragX, self.boatSailDragY, self.boatSailLiftX, self.boatSailLiftY,
+            self.boatWaterDragX, self.boatWaterDragY, self.boatWaterLiftX, self.boatWaterLiftY,
+        ]
+        data.extend(self.getWindList())
+        dataStr = [f'{x:.4f}'.rstrip('0').rstrip('.') for x in data] # FIXME very slow and inflexible
+        return ",".join(dataStr)
--- sailsim/simulation/FrameList.py
+++ sailsim/simulation/FrameList.py
@@ -7,14 +7,14 @@ class FrameList():
     def __init__(self):
         self.frames = []
 
-        self.windSize = 0
+        self.windSize = 5
         self.windDistance = 10
 
     def grabFrame(self, simulation):
         """Append new frame with all information to list."""
-        time = simulation.getTime()
         (posX, posY) = (simulation.world.boat.posX, simulation.world.boat.posY) # TODO use method of boat class when implemented
-        frame = Frame(time)
+        frame = Frame()
+        frame.collectSimulation(simulation)
         frame.collectBoat(simulation.world.boat)
         frame.collectWind(simulation.world.wind, posX, posY, self.windSize, self.windDistance)
         self.frames.append(frame)
@@ -22,14 +22,31 @@ class FrameList():
 
     def getCSV(self):
         """Generate .csv file and write it to drive."""
-        output = self.getCSVHeader()
+        output = self.getCSVHeader() + "\n"
         for frame in self.frames:
-            output += frame.getCSVLine()
+            output += frame.getCSVLine() + "\n"
         return output
 
     def getCSVHeader(self):
         """Generate head of .csv file."""
-        return "time,boatPosX,boatPosY,boatSpeedX,boatSpeedY\n"
+        headers = [
+            "frame", "time",
+            "boatPosX", "boatPosY", "boatSpeedX", "boatSpeedY", # "boatDirection",
+            "boatApparentWindX", "boatApparentWindY", "boatApparentWindAngle", "boatLeewayAngle", "boatAngleOfAttack",
+            "boatForceX", "boatForceY",
+            "boatSailDragX", "boatSailDragY", "boatSailLiftX", "boatSailLiftY",
+            "boatWaterDragX", "boatWaterDragY", "boatWaterLiftX", "boatWaterLiftY",
+        ]
+        headers.extend(self.getWindHeader())
+        return ",".join(headers)
+
+    def getWindHeader(self):
+        windHeader = []
+        for i in range(0, 2 * self.windSize + 1):
+            for j in range(0, 2 * self.windSize + 1):
+                windHeader.append("wind" + str(i) + "_" + str(j) + "X")
+                windHeader.append("wind" + str(i) + "_" + str(j) + "Y")
+        return windHeader
 
     def saveCSV(self, name="output.csv"):
         if not name.endswith(".csv"):
