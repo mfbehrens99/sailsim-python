2be2ba6344fbe9832c5984f573f0f5cdffa24989
mfbehrens99 <mfbehrens99@gmail.com>
2021-12-29 00:15:53 +0100

    Update Simulation.py

--- sailsim/simulation/Simulation.py
+++ sailsim/simulation/Simulation.py
@@ -1,10 +1,13 @@
 from copy import deepcopy
 
+from sailsim.boat import Boat
+from sailsim.wind import Wind
+
 
 class Simulation:
     """Main simulation class in this project."""
 
-    def __init__(self, boat, wind, timestep, lastFrame=None):
+    def __init__(self, boat: Boat, wind: Wind, timestep: float, lastFrame: int=None):
         """
         Create Simulation.
 
@@ -14,16 +17,16 @@ class Simulation:
             timestep:   time difference between frames
             lastFrame:  number of frames to be simulated, default: no end
         """
-        self.boat = boat
-        self.wind = wind
-        self.initBoat = deepcopy(boat)
+        self.boat: Boat = boat
+        self.wind: Wind = wind
+        self.initBoat: Boat = deepcopy(boat)
 
         # Timing
-        self.timestep = timestep
-        self.frame = 0
-        self.lastFrame = lastFrame
+        self.timestep: float = timestep
+        self.frame: int = 0
+        self.lastFrame: int = lastFrame
 
-    def run(self, steps=0):
+    def run(self, steps: int=0) -> None:
         """Run whole Simulation if lastFrame is set."""
         if steps < 1:
             # Check if lastFrame exisists
@@ -35,10 +38,10 @@ class Simulation:
             for i in range(steps):
                 self.step()
 
-    def step(self):
+    def step(self) -> None:
         """Run one step of the Simulation."""
         # Preperations
-        time = self.frame * self.timestep
+        time: float = self.frame * self.timestep
 
         # Calculate Forces on boat
         (boatX, boatY) = self.boat.getPos()                           # Fetch boat position
@@ -56,16 +59,16 @@ class Simulation:
         self.boat.moveInterval(self.timestep)
 
 
-    def getTime(self):
+    def getTime(self) -> float:
         """Return the elapsed time since the start of the simulation."""
         return self.timestep * self.frame
 
-    def totalTime(self):
+    def totalTime(self) -> float:
         """Return the total time that will elapse throughout the simulation."""
         return self.timestep * self.lastFrame
 
 
-    def reset(self):
+    def reset(self) -> None:
         """Set simulation to the first frame recorded."""
         # Reset Boat
         self.boat = deepcopy(self.initBoat)
@@ -75,12 +78,12 @@ class Simulation:
         self.frame = 0
 
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Return basic information about the simulation."""
         if self.lastFrame is None:
             return "sailsim @frm%s(%ss), %sms\n%s\n----------" % (self.frame, self.getTime(), self.timestep * 1000, self.boat, self.wind)
         return "sailsim @frm%s/%s(%ss,%s%%), %sms\n%s\n----------" % (self.frame, self.lastFrame, self.getTime(), str(round(self.frame * 100 / self.lastFrame, 1)).zfill(4), self.timestep * 1000, self.boat, self.wind)
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Return number of frames. Might be None."""
         return self.lastFrame
