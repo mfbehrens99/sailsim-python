36dabe0dd426d1c984e4f84aaafb2882a98f0526
mfbehrens99 <mfbehrens99@gmail.com>
2021-12-28 23:10:06 +0100

    Fix Simulation tests

--- tests/simulation/test_Simulation.py
+++ tests/simulation/test_Simulation.py
@@ -1,7 +1,6 @@
 """Tests module sailsim.Simulation.Simulation (and many more)."""
 
 from sailsim.simulation.Simulation import Simulation
-from sailsim.world.World import World
 from sailsim.boat.Boat import Boat
 from sailsim.wind.Wind import Wind
 from sailsim.wind.Windfield import Windfield
@@ -9,8 +8,7 @@ from sailsim.wind.Windfield import Windfield
 
 class TestSimulation():
     def setup(self):
-        world = World(Boat(), Wind([Windfield(0, 1)]), None)
-        self.s = Simulation(world, 0.01, 1)
+        self.s = Simulation(Boat(), Wind([Windfield(0, 1)]), 0.01, 1)
 
     def test_step(self):
         self.s.step()
