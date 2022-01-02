84a7996d88154e866e0d43d73416de544b69168e
mfbehrens99 <mfbehrens99@gmail.com>
2022-01-02 10:10:19 +0100

    Fix Fluctuationfield

--- sailsim/wind/Fluctuationfield.py
+++ sailsim/wind/Fluctuationfield.py
@@ -6,28 +6,24 @@ from sailsim.wind.Windfield import Windfield
 class Fluctuationfield(Windfield):
     """Field that is usually added on top of a windfield to create pseudo random fluctuations."""
 
-    def __init__(self, amplitude=1, scale=64, speed=16, x=0, y=0, noiseSeed=0):
+    def __init__(self, x=0, y=0, amplitude=1, scale=64, speed=16, noiseSeed=0):
         """
         Create a Fluctuationfield.
 
         Parameters:
+         - x:           constant wind in x direction, default: 0 m/s
+         - y:           constant wind in y direction, default: 0 m/s
          - amplitude:   scaling of the noise output. Note: output is in range ~[-0.8; 0.8], default: +-1 m/s => +-0.8 m/s
          - scale:       theoretical distance (in meters) between min and max of the fluctuations, default: 64 m
          - speed:       theoretical time (in seconds) it takes from min to max, default: 16 s
-         - x:           constant wind in x direction, default: 0 m/s
-         - y:           constant wind in y direction, default: 0 m/s
          - noiseSeed:   seed for seedX, seedY = seedX + 1, default: 0
         """
         super().__init__(x, y)
-        self.name = "Fluctuationfield"
 
         self.amplitude = amplitude
         self.scale = self.speed = None
         self.setScale(scale)
         self.setSpeed(speed)
-
-        self.speedX = x
-        self.speedY = y
 
         self.noiseSeed = noiseSeed
         self.noiseX = OpenSimplex(noiseSeed)
