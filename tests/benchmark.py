09529c957eb265953046cd5728120b48122bae44
mfbehrens99 <mfbehrens99@gmail.com>
2022-01-13 00:37:07 +0100

    Fix benchmark.py

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,8 +1,8 @@
 # Import basic modules
 from sailsim.simulation.Simulation import Simulation
-from sailsim.world.World import World
 from sailsim.boat.Boat import Boat
-from sailsim.wind.Wind import Wind
+from sailsim.sailor.Sailor import Sailor
+from sailsim.sailor.Commands import commandListExample
 
 # Import Winds
 from sailsim.wind.Windfield import Windfield
@@ -16,21 +16,19 @@ class Benchmark:
     """Test which module requires what partion of time when simulating."""
 
     def __init__(self):
-        windfield = Windfield(0, 10)
         flucfield = Fluctuationfield(2, 10, 10, 0, 0, 1200)
-        squallfield = Squallfield(0, 0, 100, 1, 0) # TODO has to be enabled later
-        wind = Wind([windfield, flucfield, squallfield])
-
         b = Boat(0, 0, 0)
-        b.setMainSailAngleDeg(45)
+        s = Sailor(commandListExample)
+        b.sailor = s
+        s.importBoat(b)
 
-        w = World(b, wind, None)
-        self.s = Simulation(w, 0.01, 1024)
+        self.s = Simulation(b, flucfield, 0.01, 1024)
+
 
     def run(self, number=10000):
         for i in range(number):
             self.s.step()
-        self.s.frameList.saveCSV(OUTPUT_PATH)
+        #self.s.frameList.saveCSV(OUTPUT_PATH)
 
 
 if __name__ == "__main__":
