7e62d2d35883868fd391b8d45f8f05363499ec45
mfbehrens99 <mfbehrens99@gmail.com>
2022-02-09 00:44:09 +0100

    type Wind

--- sailsim/wind/Wind.py
+++ sailsim/wind/Wind.py
@@ -1,3 +1,4 @@
+from typing import Union
 from sailsim.utils.coordconversion import cartToPolar
 
 from sailsim.wind.Windfield import Windfield
@@ -8,31 +9,30 @@ from sailsim.wind.Squallfield import Squallfield
 class Wind:
     """This class holds all windfields and calculates speed and direction of wind."""
 
-    def __init__(self, winds):
+    def __init__(self, winds: list[Union[Windfield, Fluctuationfield, Squallfield]]) -> None:
         self.winds = winds
 
-    def getWindCart(self, x, y, t):
-        """Summ up and returns the speed and direction of all windfields."""
-        sumX = 0
-        sumY = 0
+    def getWindCart(self, x: float, y: float, t: float) -> tuple[float, float]:
+        """Sum up and returns the speed and direction of all windfields."""
+        sumX: float = 0
+        sumY: float = 0
         for wind in self.winds:
             (windX, windY) = wind.getWindCart(x, y, t)
             sumX += windX
             sumY += windY
         return (sumX, sumY)
 
-    def getWind(self, x, y, t):
+    def getWind(self, x: float, y: float, t: float) -> tuple[float, float]:
         """Return direction and speed of the windfield at the position (x,y) as a tuple."""
         (cartX, cartY) = self.getWindCart(x, y, t)
         return cartToPolar(cartX, cartY)
 
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         # TODO make nicer
-        windfields = sum(isinstance(x, Windfield) for x in self.winds)
-        fluctuationfields = sum(type(x) is Fluctuationfield for x in self.winds)
-        squallfields = sum(type(x) is Squallfield for x in self.winds)
+        windfields: int = sum(isinstance(x, Windfield) for x in self.winds)
+        fluctuationfields: int = sum(type(x) is Fluctuationfield for x in self.winds)
+        squallfields: int = sum(type(x) is Squallfield for x in self.winds)
         return f"Wind made up of {len(self.winds)} winds:\n\t{windfields} Windfields\n\t{fluctuationfields} Fluctuationfields\n\t{squallfields} Squallfields"
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.winds)
--- sailsim/wind/Windfield.py
+++ sailsim/wind/Windfield.py
@@ -6,19 +6,19 @@ from sailsim.utils.coordconversion import cartToPolar
 class Windfield:
     """Describe a partion of the wind."""
 
-    def __init__(self, x, y):
-        self.speedX = x
-        self.speedY = y
+    def __init__(self, x: float, y: float) -> None:
+        self.speedX: float = x
+        self.speedY: float = y
 
-    def getWindCart(self, _x=0, _y=0, _t=0):
+    def getWindCart(self, _x: float = 0, _y: float = 0, _t: float = 0) -> tuple[float, float]:
         """Return cartesian components of the windfield at the position (x, y) as a tuple."""
         return (self.speedX, self.speedY)
 
-    def getWind(self, _x=0, _y=0, _t=0):
+    def getWind(self, _x: float = 0, _y: float = 0, _t: float = 0) -> tuple[float, float]:
         """Return direction and speed of the windfield at the position (x, y) as a tuple."""
         (cartX, cartY) = self.getWindCart()
         return cartToPolar(cartX, cartY)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         (speed, direction) = self.getWind()
         return f"Windfield: {speed}m/s @{direction * 180 / pi}"
